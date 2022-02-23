d58d015ec404f5221973accc32a7504f6050d62c
Ivan Voronov <sheroe@yandex.ru>
2022-02-23 16:46:57 +0300

    Initial commit

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int input, i;
+    cin >> input;
+    input = (input * 3 + 2); //типо мат операции вот :)
+    int* output = new int[input];
+    for (i = 0; i < input; i++)
+    {
+        output[i] = -100 + rand() % 200;
+        cout << output[i] << endl;
+    }
+
+    delete[] output;
+    system("pause");
+    return 0;
+}
